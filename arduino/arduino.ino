19442d1e72d308f01f10a7c49a4915299cf63772
Laaouatni Anas <laaouatni.anas@outlook.it>
2025-02-23 18:03:11 +0100

    now with .as<String>() it shows, but to remove the low memory error I commented at least for now the display library NeoMatrix

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -2,14 +2,14 @@
 #include <ArduinoJson.h>
 #include <Adafruit_NeoMatrix.h>
 
-Adafruit_NeoMatrix thisPannello = Adafruit_NeoMatrix(32,8,6,NEO_MATRIX_COLUMNS + NEO_MATRIX_ZIGZAG);
+// Adafruit_NeoMatrix thisPannello = Adafruit_NeoMatrix(32,8,6,NEO_MATRIX_COLUMNS + NEO_MATRIX_ZIGZAG);
 SoftwareSerial Serial2(2, 3);  // RX, TX
 
 void setup() {
+  // thisPannello.begin();
   Serial2.begin(4800);
   Serial.begin(4800);
-  thisPannello.begin();
-  thisPannello.setBrightness(10);
+  // thisPannello.setBrightness(10);
 }
 
 void loop() {
@@ -28,38 +28,40 @@ void myCallback(String esp32value) {
     return;
   };
 
+  Serial.println("xLine: " + doc["xLine"].as<String>());
+
   // for(int y = 0; y < doc.size(); y++) {
   //   for(int x = 0; x < doc[y].size(); x++) {
   //     Serial.println(doc[y][x].as<String>());
   //   }
   // }
 
-  for(int thisY = 0; thisY < 8; thisY++) {
-    int R,G,B;
-    // if(doc["colorArray"][thisY].size() < 3) {
-      R = 255;
-      G = 255;
-      B = 255;
-    // } else {
-    //   R = doc["colorArray"][thisY][0];
-    //   G = doc["colorArray"][thisY][1];
-    //   B = doc["colorArray"][thisY][2];
-    // };
+  // for(int thisY = 0; thisY < 8; thisY++) {
+  //   int R,G,B;
+  //   // if(doc["colorArray"][thisY].size() < 3) {
+  //     R = 255;
+  //     G = 255;
+  //     B = 255;
+  //   // } else {
+  //   //   R = doc["colorArray"][thisY][0];
+  //   //   G = doc["colorArray"][thisY][1];
+  //   //   B = doc["colorArray"][thisY][2];
+  //   // };
 
-    Serial.println(String(R) + "\t" + String(G) + "\t" + String(B));
+  //   Serial.println(String(R) + "\t" + String(G) + "\t" + String(B));
 
-    thisPannello.drawPixel(
-      doc["xLine"], 
-      thisY, 
-      thisPannello.Color(R,G,B)
-    );
-  };
+  //   thisPannello.drawPixel(
+  //     doc["xLine"], 
+  //     thisY, 
+  //     thisPannello.Color(R,G,B)
+  //   );
+  // };
 
-  thisPannello.show();
+  // thisPannello.show();
 
   // Serial.println("⬅️ ho ricevuto un messaggio: " + esp32value);
 
-  // doc.clear();
+  doc.clear();
 };
 
 String listenToNewSerialData(void (*myCallback)(String)) {
