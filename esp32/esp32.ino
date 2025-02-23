5032deae5e167c9f620f6215e2d94e534b394a16
Laaouatni Anas <laaouatni.anas@outlook.it>
2025-02-23 19:07:58 +0100

    save

--- esp32/esp32.ino
+++ esp32/esp32.ino
@@ -1,13 +1,18 @@
 #include <AsyncTCP.h>
 #include <ESPAsyncWebServer.h>
 #include <WiFi.h>
+#include <ArduinoJson.h>
+#include <Adafruit_NeoMatrix.h>
 
 AsyncWebServer server(80);
 AsyncWebSocket ws("/ws");
 
+Adafruit_NeoMatrix thisPannello = Adafruit_NeoMatrix(32,8,6,NEO_MATRIX_COLUMNS + NEO_MATRIX_ZIGZAG);
+JsonDcoument doc;
+
 void setup() {
   Serial.begin(4800);
-  Serial2.begin(4800, SERIAL_8N1, 16, 17);
+  // Serial2.begin(4800, SERIAL_8N1, 16, 17);
   WiFi.begin("nomeWifi", "passwordWifi");
 
   while (WiFi.status() != WL_CONNECTED) {};
@@ -30,9 +35,10 @@ void setup() {
 
     const String thisStringData = String((char *)data).substring(0, len);
 
-    ws.textAll(thisStringData);
-    Serial2.println(thisStringData);
-    Serial.println("⬅️ ho ricevuto dal CLIENT un messaggio e ➡️ inoltrato ai altri: " + String(thisStringData));
+
+    // ws.textAll(thisStringData);
+    // Serial2.println(thisStringData);
+    // Serial.println("⬅️ ho ricevuto dal CLIENT un messaggio e ➡️ inoltrato ai altri: " + String(thisStringData));
   });
 
   server.addHandler(&ws);
@@ -41,29 +47,29 @@ void setup() {
 
 void loop() {
   ws.cleanupClients();
-  listenToNewSerialData(&myCallback);
+  // listenToNewSerialData(&myCallback);
 };
 
-void myCallback(String arduinoValue) {
-  Serial.println("⬅️ ho ricevuto da ARDUINO un messaggio e ➡️ inoltrato ai altri: " + arduinoValue);
-  ws.textAll(arduinoValue);
-}
+// void myCallback(String arduinoValue) {
+//   Serial.println("⬅️ ho ricevuto da ARDUINO un messaggio e ➡️ inoltrato ai altri: " + arduinoValue);
+//   ws.textAll(arduinoValue);
+// }
 
-String listenToNewSerialData(void (*myCallback)(String)) {
-  static String receivedString;
+// String listenToNewSerialData(void (*myCallback)(String)) {
+//   static String receivedString;
 
-  while (Serial2.available() > 0) {
-    const char thisChar = Serial2.read();
-    const bool isLastChar = thisChar == '\n';
+//   while (Serial2.available() > 0) {
+//     const char thisChar = Serial2.read();
+//     const bool isLastChar = thisChar == '\n';
 
-    if (!isLastChar) {
-      receivedString += thisChar;
-      continue;
-    };
+//     if (!isLastChar) {
+//       receivedString += thisChar;
+//       continue;
+//     };
 
-    myCallback(receivedString);
-    receivedString = "";
-  };
+//     myCallback(receivedString);
+//     receivedString = "";
+//   };
 
-  return "";
-};
+//   return "";
+// };
