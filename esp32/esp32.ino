60954c81985440c4c5971a67f3e253f050fecb81
Laaouatni Anas <laaouatni.anas@outlook.it>
2025-03-03 16:38:09 +0100

    try to solve this: Access to fetch at 'http://192.168.194.3/updateDisplayPixels' from origin 'http://127.0.0.1:5500' has been blocked by CORS policy: Request header field content-type is not allowed by Access-Control-Allow-Headers in preflight response.

--- esp32/esp32.ino
+++ esp32/esp32.ino
@@ -21,40 +21,48 @@ void setup() {
   
   DefaultHeaders::Instance().addHeader("Access-Control-Allow-Origin", "*");
   // DefaultHeaders::Instance().addHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
-  // DefaultHeaders::Instance().addHeader("Access-Control-Allow-Headers", "Content-Type");
+  DefaultHeaders::Instance().addHeader("Access-Control-Allow-Headers", "Content-Type");
 
-  server.onNotFound([](AsyncWebServerRequest* request) {
-    if(request->method() == HTTP_OPTIONS) {
-      request->send(200);
-      return;
-    } 
-
-    if(request->method() == HTTP_POST) {
-      if(request->url() == "/updateDisplayPixels") {
-        request->onBody([](AsyncWebServerRequest *request, uint8_t *data, size_t len, size_t index, size_t total) {
-          DeserializationError error = deserializeJson(doc, data);
-          JsonArray pixelArrayColors = doc["colorMatrixData"]["arrayColors"];
-          
-          for(int forY = 0; forY < 8; forY++) {
-            JsonArray arrayRigaPannello = pixelArrayColors[forY];
-            for(int forX = 0; forX < 32; forX++) {
-              JsonObject pixelColor = arrayRigaPannello[forX];
-              int R = pixelColor["R"];
-              int G = pixelColor["G"];
-              int B = pixelColor["B"];
-              thisPannello.drawPixel(forX, forY, thisPannello.Color(R,G,B));
-            };
-          }
-          thisPannello.show();
-          doc.clear();
-        });
-        request->send(200, "text/plain", "Display updated");
-        return;
-      }
-    }
-
-    request->send(404,"text/plain", "send the correct POST request instead");
+  server.on("/updateDisplayPixels", HTTP_OPTIONS, [](AsyncWebServerRequest *request) {
+    request->send(200, "text/plain", "OK. CORS bypassed");
   });
+
+  server.on("/updateDisplayPixels", HTTP_POST, [](AsyncWebServerRequest *request) {
+    request->send(200, "text/plain", "YES. POST is working");
+  });
+
+  // server.onNotFound([](AsyncWebServerRequest* request) {
+  //   if(request->method() == HTTP_OPTIONS) {
+      
+  //     return;
+  //   } 
+
+  //   if(request->method() == HTTP_POST) {
+  //     if(request->url() == "/updateDisplayPixels") {
+  //       request->onBody([](AsyncWebServerRequest *request, uint8_t *data, size_t len, size_t index, size_t total) {
+  //         DeserializationError error = deserializeJson(doc, data);
+  //         JsonArray pixelArrayColors = doc["colorMatrixData"]["arrayColors"];
+          
+  //         for(int forY = 0; forY < 8; forY++) {
+  //           JsonArray arrayRigaPannello = pixelArrayColors[forY];
+  //           for(int forX = 0; forX < 32; forX++) {
+  //             JsonObject pixelColor = arrayRigaPannello[forX];
+  //             int R = pixelColor["R"];
+  //             int G = pixelColor["G"];
+  //             int B = pixelColor["B"];
+  //             thisPannello.drawPixel(forX, forY, thisPannello.Color(R,G,B));
+  //           };
+  //         }
+  //         thisPannello.show();
+  //         doc.clear();
+  //       });
+  //       request->send(200, "text/plain", "Display updated");
+  //       return;
+  //     }
+  //   }
+
+  //   request->send(404,"text/plain", "send the correct POST request instead");
+  // });
 
   server.begin();
 }
