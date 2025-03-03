37fb2bfcbf29a742e6e3b55960787442cdc63c4f
Laaouatni Anas <laaouatni.anas@outlook.it>
2025-03-03 16:40:19 +0100

    FINALLY I GET 200 IN POST REQUEST. HERE THE LEAST POSSIBLE CODE

--- esp32/esp32.ino
+++ esp32/esp32.ino
@@ -20,7 +20,6 @@ void setup() {
   Serial.println("WiFi connected! IP Address: " + WiFi.localIP().toString());
   
   DefaultHeaders::Instance().addHeader("Access-Control-Allow-Origin", "*");
-  // DefaultHeaders::Instance().addHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
   DefaultHeaders::Instance().addHeader("Access-Control-Allow-Headers", "Content-Type");
 
   server.on("/updateDisplayPixels", HTTP_OPTIONS, [](AsyncWebServerRequest *request) {
@@ -30,39 +29,6 @@ void setup() {
   server.on("/updateDisplayPixels", HTTP_POST, [](AsyncWebServerRequest *request) {
     request->send(200, "text/plain", "YES. POST is working");
   });
-
-  // server.onNotFound([](AsyncWebServerRequest* request) {
-  //   if(request->method() == HTTP_OPTIONS) {
-      
-  //     return;
-  //   } 
-
-  //   if(request->method() == HTTP_POST) {
-  //     if(request->url() == "/updateDisplayPixels") {
-  //       request->onBody([](AsyncWebServerRequest *request, uint8_t *data, size_t len, size_t index, size_t total) {
-  //         DeserializationError error = deserializeJson(doc, data);
-  //         JsonArray pixelArrayColors = doc["colorMatrixData"]["arrayColors"];
-          
-  //         for(int forY = 0; forY < 8; forY++) {
-  //           JsonArray arrayRigaPannello = pixelArrayColors[forY];
-  //           for(int forX = 0; forX < 32; forX++) {
-  //             JsonObject pixelColor = arrayRigaPannello[forX];
-  //             int R = pixelColor["R"];
-  //             int G = pixelColor["G"];
-  //             int B = pixelColor["B"];
-  //             thisPannello.drawPixel(forX, forY, thisPannello.Color(R,G,B));
-  //           };
-  //         }
-  //         thisPannello.show();
-  //         doc.clear();
-  //       });
-  //       request->send(200, "text/plain", "Display updated");
-  //       return;
-  //     }
-  //   }
-
-  //   request->send(404,"text/plain", "send the correct POST request instead");
-  // });
 
   server.begin();
 }
