62aa958d9e3da1a4401f62362188373e974c5000
Laaouatni Anas <laaouatni.anas@outlook.it>
2025-03-03 16:19:34 +0100

    save

--- esp32/esp32.ino
+++ esp32/esp32.ino
@@ -55,7 +55,23 @@ void setup() {
 
     if(request->method() == HTTP_POST) {
       if(request->url() == "/updateDisplayPixels") {
-
+        request->onBody([](AsyncWebServerRequest *request, uint8_t *data, size_t len, size_t index, size_t total) {
+          DeserializationError error = deserializeJson(doc, data);
+          JsonArray pixelArrayColors = doc["colorMatrixData"]["arrayColors"];
+          
+          for(int forY = 0; forY < 8; forY++) {
+            JsonArray arrayRigaPannello = pixelArrayColors[forY];
+            for(int forX = 0; forX < 32; forX++) {
+              JsonObject pixelColor = arrayRigaPannello[forX];
+              int R = pixelColor["R"];
+              int G = pixelColor["G"];
+              int B = pixelColor["B"];
+              thisPannello.drawPixel(forX, forY, thisPannello.Color(R,G,B));
+            };
+          }
+          thisPannello.show();
+          doc.clear();
+        });
         request->send(200, "text/plain", "Display updated");
         return;
       }
