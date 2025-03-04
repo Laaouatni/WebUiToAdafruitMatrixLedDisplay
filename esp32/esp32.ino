9dd9313da5978e0a5deb1ae553ad2a64174b48e9
Laaouatni Anas <laaouatni.anas@outlook.it>
2025-03-04 16:43:34 +0100

    starting to remove the ArduinoJson to create a faster version and efficient version in the next commits

--- esp32/esp32.ino
+++ esp32/esp32.ino
@@ -1,13 +1,11 @@
 #include <WiFi.h>
-#include <ArduinoJson.h>
+// #include <ArduinoJson.h>
 #include <Adafruit_NeoMatrix.h>
 #include <ESPAsyncWebServer.h>
 
 AsyncWebServer server(80);
 
 Adafruit_NeoMatrix thisPannello = Adafruit_NeoMatrix(32,8,15,NEO_MATRIX_COLUMNS + NEO_MATRIX_ZIGZAG);
-JsonDocument doc;
-
 String completeJsonString = "";
 
 void setup() {
@@ -33,29 +31,29 @@ void setup() {
     [](AsyncWebServerRequest *request, uint8_t *data, size_t len, size_t index, size_t total) {
       const String thisStringData = String((char*)data).substring(0, len);
       completeJsonString += thisStringData;
+      if (index + len != total) return;
 
-      if(index+len == total) {
-        DeserializationError error = deserializeJson(doc, completeJsonString);
-        if(error) {
-          request->send(400, "text/plain", "JSON Parsing Error: " + String(error.c_str()));
-          return;
-        };
-        JsonArray pixelArrayColors = doc["arrayColors"];
-        for(int forY = 0; forY < 8; forY++) {
-          JsonArray arrayRigaPannello = pixelArrayColors[forY];
-          for(int forX = 0; forX < 32; forX++) {
-            JsonArray pixelColor = arrayRigaPannello[forX];
-            int R = pixelColor[0];
-            int G = pixelColor[1];
-            int B = pixelColor[2];
-            thisPannello.drawPixel(forX, forY, thisPannello.Color(R,G,B));
-          };
-        };
-        thisPannello.show();
-        doc.clear();
-        completeJsonString = "";
-        request->send(200, "text/plain", "Display updated");
+      // DeserializationError error = deserializeJson(doc, completeJsonString);
+      if(error) {
+        request->send(400, "text/plain", "JSON Parsing Error: " + String(error.c_str()));
+        return;
       };
+      // JsonArray pixelArrayColors = doc["arrayColors"];
+      for(int forY = 0; forY < 8; forY++) {
+        // JsonArray arrayRigaPannello = pixelArrayColors[forY];
+        for(int forX = 0; forX < 32; forX++) {
+          // JsonArray pixelColor = arrayRigaPannello[forX];
+          int R = pixelColor[0];
+          int G = pixelColor[1];
+          int B = pixelColor[2];
+          thisPannello.drawPixel(forX, forY, thisPannello.Color(R,G,B));
+        };
+      };
+      thisPannello.show();
+      doc.clear();
+      completeJsonString = "";
+      request->send(200, "text/plain", "Display updated");
+      
   });
 
   server.begin();
